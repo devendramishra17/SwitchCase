706b27a22da4dda76d0a03f1a1b185b7b54d9776
devendramishra17 <devendramishra17@gmail.com>
2021-03-29 10:40:06 +0530

    Initial commit

---
+++ src/com/indianappexpress/Main.java
@@ -0,0 +1,30 @@
+package com.indianappexpress;
+/**
+* Author : Devendra Mishra
+*/
+public class Main {
+
+    public static void main(String[] args) {
+     //print Your phone Brand Name
+     String brand = "Motorola";
+     Switch (brand) {
+
+            case "Apple":
+                System.out.println("Apple Phone");
+               break;
+            case "Samsung":
+                System.out.println("Samsung Phone");
+                break;
+            case "Motorola" :
+                System.out.println("Motorola Phone");
+                break;
+            case "Nokia" :
+                System.out.println("Nokia phone");
+                break;
+            default :
+                System.out.println("Use Best phone brand ");
+                break;
+     }
+
+    }
+}
